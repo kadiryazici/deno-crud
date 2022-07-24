6b7f68f51578f3c2a72a439658947780da4667d9
kadiryazici <kyzc411@outlook.com.tr>
2022-07-24 22:26:03 +0300

    Add bcrypt and refactor code

--- src/services/helpers.service.ts
+++
@@ -1,8 +0,0 @@
-import { getFixedUsername, getRoot, getUTCNow, resultFromCb } from 'helpers';
-
-export class HelpersService {
-  public getFixedUsername = getFixedUsername;
-  public getRoot = getRoot;
-  public getUTCNow = getUTCNow;
-  public resultFromCb = resultFromCb;
-}
